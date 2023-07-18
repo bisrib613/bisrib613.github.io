06a13e8bad62badee70ed89bea70e6894b25802c
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2023-07-18 22:48:56 +0700

    Create eka50.js

---
+++ eka50.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '1e70f9a3d4c43fe883c1af51213212d5',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/1e70f9a3d4c43fe883c1af51213212d5/invoke.js"></scr' + 'ipt>');
