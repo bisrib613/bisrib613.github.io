933f371e82c4e18286535642f9294be3b27bceda
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2023-07-18 22:49:38 +0700

    Create bima50.js

---
+++ bima50.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '35866accaae8ce20b6fb64628c2bb201',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/35866accaae8ce20b6fb64628c2bb201/invoke.js"></scr' + 'ipt>');
